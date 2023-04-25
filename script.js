f285cb8282c2a6780a9b3854d03ede6fd4b66643
John Vu <johntrinhvu@yahoo.com>
2023-04-25 16:33:07 -0700

    Changed red to h2 and blue to h3, as well as add some functions renderMessage and renderControl for resetBtn

--- script.js
+++ script.js
@@ -1,6 +1,69 @@
+// John Vu
+// 28 April 2023
+// Checkers JS File
+
+/* Constant Variables */
 const PLAYERS = {
     "0": null,
-    "1": "red",
-    "-1": "blue"
+    "1": "RED",
+    "-1": "BLUE"
+
+}
+
+const COLORS = {
+    "1": "#ff6961", // light red
+    "-1": "#0096FF", // light blue
+    "draw": "#C4A484" // color when there is Draw
+  
+}
+
+/* State Variables */
+const board = [
+    null, 0, null, 1, null, 2, null, 3, null, 4, null, 5, null, 6, null, 7, 
+    null, 8, null, 9, null, 10, null, 11, null, 12, null, 13, null, 14, null, 15, 
+    null, 16, null, 17, null, 18, null, 19, null, 20, null, 21, null, 22, null, 23, null 
+
+]
+
+let turn;
+let winner;
+
+/* Cached Elements */
+const allSpaces = document.querySelectorAll("td");
+const messageEl = document.querySelector("h1");
+let redObjects = document.querySelectorAll("h2");
+let blueObjects = document.querySelectorAll("h3");
+const resetBoardBtn = document.querySelector("button");
+
+/* Event Listeners */
+
+
+/* Functions */
+function render() {
+    // renderBoard();
+    renderMessage();
+    renderControls();
+
+}
+
+function renderMessage() {
+    // if tie
+    if (winner === "T") {
+        messageEl.innerHTML = `<span style="color: ${COLORS.draw};font-size:4vmin">DRAW</span>`
     
+    
+    } else if (winner) {
+        // we have winner
+        messageEl.innerHTML = `<span style="color: ${COLORS[winner]};font-size:4vmin">${PLAYERS[winner]}</span> WINS!`
+    
+    } else {
+        // Game is still in play
+        messageEl.innerHTML = `<span style="color: ${COLORS[turn]};font-size:4vmin">${PLAYERS[turn]}</span>'s TURN`
+    }
+
+}
+
+function renderControls() {
+    resetBoardBtn.style.visibility = winner ? "visible" : "hidden";
+
 }
