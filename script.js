614d6ccea881c49899a2e100441b3d80bbaf1f1c
John Vu <johntrinhvu@yahoo.com>
2023-04-26 00:52:59 -0700

    Made so I can highlight over piece, when clicked it will highlight it

--- script.js
+++ script.js
@@ -4,22 +4,22 @@
 
 /* Constant Variables */
 const PLAYERS = {
-    "0": null,
     "1": {
-        "name": "BLUE", 
+        "name": "blue", 
         "color": "#0096FF", // light blue
         "score": 12,
         "identity": 1
 
     },
     "-1": {
-        "name": "RED",
+        "name": "red",
         "color": "#ff6961", // light red
         "score": 12,
         "id": -1
         
     },
-    "draw": "#C4A484" // color for Draw
+    "draw": "#C4A484", // color for Draw
+    "selected": "#bdbd87"
 
 }
 
@@ -30,13 +30,30 @@ let board; // an array of arrays of 8x8 - 64 total spaces
 let winner; // wiiner has to be blue or red, T for draw
 
 /* Cached Elements */
-const cells = document.querySelectorAll("div");
+const cells = [...document.querySelectorAll("div")];
 const messageEl = document.querySelector("h1");
 let redPieces = document.querySelectorAll("h3");
 let bluePieces = document.querySelectorAll("h4");
+let selectedPiece; // current selected piece
 const resetBoardBtn = document.querySelector("button");
 
 /* Event Listeners */
+redPieces.forEach(function(redPiece) {
+    redPiece.addEventListener('click', handleMove)
+    
+  
+});
+
+bluePieces.forEach(function(bluePiece) {
+    bluePiece.addEventListener('click', handleMove)
+  
+});
+
+// cells.forEach(function(cell) {
+//     cell.addEventListener('click', handleMove)
+  
+// });
+
 
 
 // call init
@@ -44,8 +61,8 @@ init();
 
 /* Functions */
 function init() {
+
     // to visualize the board's mapping to the DOM, rotate 90 degrees counter-clockwise
-  
     board = [
         [1, 0, 1, 0, 0, 0, -1, 0], // col 0
         [0, 1, 0, 0, 0, -1, 0, -1], // col 1
@@ -57,11 +74,35 @@ function init() {
         [0, 1, 0, 0, 0, -1, 0, -1], // col 7
 
     ];
+
     turn = 1;
     winner = null;
     render();
     
 }
+
+function handleMove(evt) {
+    const clickedPiece = evt.target;
+    const clickedPieceId = evt.target.id;
+
+    // if there alr is selected piece, remove from the color and set to null
+    if (selectedPiece) {
+        selectedPiece.classList.remove("pieceSelected");
+        selectedPiece = null;
+
+    }
+
+    // else if the piece is by the current player, it will add it to the css element
+    if (clickedPiece.classList.contains(`${PLAYERS[turn].name}`)) {
+        selectedPiece = clickedPiece;
+        clickedPiece.classList.add("pieceSelected");
+        // then highlight possible moves
+        spotPossibleMoves()
+        console.log(clickedPieceId);
+    }
+
+}
+  
 
 function render() {
     // renderBoard();
@@ -70,6 +111,22 @@ function render() {
 
 }
 
+// function renderBoard() {
+//     board.forEach(function(colArr, colIdx) {
+//       colArr.forEach(function(cellVal, rowIdx) {
+//         const cellId = `c${colIdx}r${rowIdx}`;
+//         const cellEl = document.getElementById(cellId);
+//         if (PLAYERS[cellVal].identity === 1) {
+//             return;
+
+//         } else if (PLAYERS[cellVal].identity === -1) {
+//             return;
+
+//         } 
+  
+//       });
+//     });
+//   }
 
 function renderMessage() {
     // if tie
@@ -79,11 +136,11 @@ function renderMessage() {
     
     } else if (winner) {
         // we have winner
-        messageEl.innerHTML = `<span style="color: ${PLAYERS[winner].color}">${PLAYERS[winner].name}</span> WINS!`
+        messageEl.innerHTML = `<span style="color: ${PLAYERS[winner].color}">${PLAYERS[winner].name.toUpperCase()}</span> WINS!`
     
     } else {
         // Game is still in play
-        messageEl.innerHTML = `<span style="color: ${PLAYERS[turn].color}">${PLAYERS[turn].name}</span>'s TURN`
+        messageEl.innerHTML = `<span style="color: ${PLAYERS[turn].color}">${PLAYERS[turn].name.toUpperCase()}</span>'s TURN`
     }
 
 }
